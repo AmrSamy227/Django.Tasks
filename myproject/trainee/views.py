4b77801552fc52d392f02b417bfe5f99dd53fa13
AmrSamy227 <amrsamihst@gmail.com>
2025-09-22 16:46:14 +0300

    Add files via upload

---
+++ myproject/trainee/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def alltrainees(request):
+    trainees = [
+        [1, 'Amr', 'Python'],
+        [2, 'Sara', 'Java'],
+        [3, 'Ali', 'C#']
+    ]
+    return render(request, 'trainee/list.html', context={'trainees': trainees})
+
+
+def gettraineeid(request):
+    return HttpResponse("<h1>welcome to this trainee</h1>")
+
+def inserttrainee(request):
+    return HttpResponse("<h1>welcome to insert trainee</h1>")
+
+def updatetrainee(request, id):
+    return HttpResponse(f"<h1>welcome to update trainee {id}</h1>")
+
+def deletetrainee(request, id):
+    return HttpResponse(f"<h1>welcome to delete trainee {id}</h1>")
